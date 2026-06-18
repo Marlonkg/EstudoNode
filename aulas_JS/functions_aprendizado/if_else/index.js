f1bce53a6745247fa1a0091a4f68b81bce968eb5
Marlon Marques <marlonmarques2002@gmail.com>
2026-06-18 18:22:46 -0300

    if else if if else elseif elif

---
+++ aulas_JS/functions_aprendizado/if_else/index.js
@@ -0,0 +1,28 @@
+//teste de idade mais antigo que sua vó
+let idade = 10
+let nome = "maria"
+
+if(idade >= 18 && nome == "Jão" || nome == 'Andressa' || nome == 'Maria'){
+    console.log('voce pode entrar')
+} else{
+    console.log('voce não pode entrar')
+}
+
+
+let passaporte = false
+let rg = true
+
+//segundo exemplo usando a separação de parametros
+if(nome == "Jão" && idade >=18 && (passaporte == true || rg == true) ){
+    console.log(`olá ${nome}, voce pode entrar.`)
+}else{
+    console.log(`Desculpe ${nome}, mas voce não tem permissão para entrar!`)
+}
+
+if(nome == undefined){
+    console.log("o nome não foi inserido corretamente")
+} else if(nome.length <= 4){
+    console.log("o nome é muito pequeno, tente outro")
+} else{
+    console.log("nome inserido com sucesso")
+}
