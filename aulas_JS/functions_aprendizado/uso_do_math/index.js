426ef2aac26d5f23431517776bcc10b2162d3c2a
Marlon Marques <marlonmarques2002@gmail.com>
2026-06-18 20:41:36 -0300

    potencia

--- aulas_JS/functions_aprendizado/uso_do_math/index.js
+++ aulas_JS/functions_aprendizado/uso_do_math/index.js
@@ -11,3 +11,17 @@ let arredondar = Math.round(3.4) //arredonda para onde a maior parte pender
 console.log(arredondar)
 let arredondarParaCima = Math.ceil(5.2)// sempre pra cima
 console.log(arredondarParaCima)
+
+// pow é para o resultado de um numero elevado a potencias com dois paramentro sendo o segundo a pontencia devida
+console.log('o resultado de 5² é ' + Math.pow(5, 2))
+console.log('o resultado de 3² é ' + Math.pow(3, 2))
+console.log('o resultado de 18² é ' + Math.pow(18, 2))
+
+
+// criando variavel antes, mesmo resultado
+let n1 = Math.pow(5, 2)
+console.log(`5² = ${n1}`)
+
+// posso usar a função dentro da interpolação e mencionar variaveis dentro dela.
+let n2 = 3
+console.log(`${n2}² = ${Math.pow(n2, 2)}`)
