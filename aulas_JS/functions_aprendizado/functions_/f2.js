bde3d2ca8d3149fabd4b2cdbc6a5939dda18705f
Marlon Marques <marlonmarques2002@gmail.com>
2026-06-20 18:43:46 -0300

    Update f2.js

--- aulas_JS/functions_aprendizado/functions_/f2.js
+++ aulas_JS/functions_aprendizado/functions_/f2.js
@@ -9,65 +9,38 @@ inquirer.prompt([
     }
 ]).then((resposta) =>{
     if (resposta.p1 == 1){
-        inquirer.prompt([
-            {
-                name: 'n1', message: 'primeiro numero:\n'
-            },{
-                name: 'n2', message: 'segundo numero:\n'
-            }
-        ]).then((num) =>{
-            let a = parseInt(num.n1)
-            let b = parseInt(num.n2)
-            console.log(`${mais(a, b)}`)
-        }).catch(err => console.log(err))
 
     }else if (resposta.p1 == 2){
 
-        inquirer.prompt([
-            {
-                name: 'n1', message: 'primeiro numero:\n'
-            },{
-                name: 'n2', message: 'segundo numero:\n'
-            }
-        ]).then((num) =>{
-            let a = parseInt(num.n1)
-            let b = parseInt(num.n2)
-            console.log(`${menos(a, b)}`)
-        }).catch(err => console.log(err))
-
     }else if (resposta.p1 == 3){
 
-        inquirer.prompt([
-            {
-                name: 'n1', message: 'primeiro numero:\n'
-            },{
-                name: 'n2', message: 'segundo numero:\n'
-            }
-        ]).then((num) =>{
-            let a = parseInt(num.n1)
-            let b = parseInt(num.n2)
-            console.log(`${mult(a, b)}`)
-        }).catch(err => console.log(err))
-
     }else if (resposta.p1 == 4){
-
-        inquirer.prompt([
-            {
-                name: 'n1', message: 'primeiro numero:\n'
-            },{
-                name: 'n2', message: 'segundo numero:\n'
-            }
-        ]).then((num) =>{
-            let a = parseInt(num.n1)
-            let b = parseInt(num.n2)
-            console.log(`${div(a, b)}`)
-        }).catch(err => console.log(err))
 
     }else {
         console.log('resposta invalida')
     }
 }).catch(err => console.log(err))
 
+
+//capturando os dois numeros
+function doisN(digito){
+    inquirer.prompt([
+    {
+        name: 'n1', message: 'primeiro numero:\n'
+    },{
+        name: 'n2', message: 'segundo numero:\n'
+     }
+]).then((num) =>{
+    let a = parseInt(num.n1)
+    let b = parseInt(num.n2)
+    
+    if(a == NaN && b == NaN){
+        console.log('Não foi aceito, digite apenas numeros inteiros.')
+    } else if(digito == 1) {
+        console.log(mais(a, b))
+    } else if(digito == 2)
+}).catch(err => console.log(err))
+}
 
 function mais(a, b){
     return a + b
