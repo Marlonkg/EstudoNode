8c3afbfd6fb222939b57e6a2d5cd70acf671dbd9
Marlon Marques <marlonmarques2002@gmail.com>
2026-06-21 02:53:23 -0300

    att da calculadora -- em progresso

--- aulas_JS/functions_aprendizado/functions_/f2.js
+++ aulas_JS/functions_aprendizado/functions_/f2.js
@@ -1,46 +1,56 @@
 const inquirer = require('inquirer')
 //isso deveria estar em teste pessoal, mas ja instalei a dependencias aqui ent vai aq mesmo 
 
+//melhorando mini calculadora desnecessariamente grande
+// IS COMMING...
+async function calculadora() {
+    const pergunta = [
+        {
+            type: 'list',
+            name: 'op',
+            message: 'Qual operação deseja?',
+            choices: ['Adição', 'Subtração', 'Multiplicação', 'Divisão']
+        }
+    ]
+    try {
 
-// mini calculadora desnecessariamente grande
-inquirer.prompt([
-    {
-        name: 'p1', message: 'Qual operação deseja fazer?\n 1- Adição \n 2- subtração \n 3- multiplicação \n 4- divisão \n'
-    }
-]).then((resposta) =>{
-    if (resposta.p1 == 1){
-
-    }else if (resposta.p1 == 2){
-
-    }else if (resposta.p1 == 3){
-
-    }else if (resposta.p1 == 4){
-
-    }else {
-        console.log('resposta invalida')
-    }
-}).catch(err => console.log(err))
-
-
-//capturando os dois numeros
-function doisN(digito){
-    inquirer.prompt([
-    {
-        name: 'n1', message: 'primeiro numero:\n'
-    },{
-        name: 'n2', message: 'segundo numero:\n'
-     }
-]).then((num) =>{
-    let a = parseInt(num.n1)
-    let b = parseInt(num.n2)
+        const resposta = await inquirer.prompt(pergunta)
+        console.log(resposta)
+        return doisN(resposta)
     
-    if(a == NaN && b == NaN){
-        console.log('Não foi aceito, digite apenas numeros inteiros.')
-    } else if(digito == 1) {
-        console.log(mais(a, b))
-    } else if(digito == 2)
-}).catch(err => console.log(err))
+    } catch (error) {
+
+        console.error('ocorreu um erro', error)
 }
+}
+
+
+async function doisN() {
+    const pergunta = [
+        {
+            type: 'number',
+            name: 'n1',
+            message: 'Digite o primeiro numero: \n'
+        },{
+            type: 'number',
+            name: 'n2',
+            message: 'Digite o segundo numero: \n'
+        }
+    ]
+    try {
+
+        const resposta = await inquirer.prompt(pergunta)
+        (resposta.n1, resposta.n2)
+
+    } catch (error) {
+        console.error('ocorreu um erro', error)
+    }
+    
+}
+
+
+calculadora()
+
 
 function mais(a, b){
     return a + b
