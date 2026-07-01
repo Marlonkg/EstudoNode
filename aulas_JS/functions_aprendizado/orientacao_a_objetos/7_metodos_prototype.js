6a8ea7e6ef0da607521693b102737bdc8592e22a
Marlon Marques <marlonmarques2002@gmail.com>
2026-07-01 17:28:45 -0300

    organização do objeto

---
+++ aulas_JS/functions_aprendizado/orientacao_a_objetos/7_metodos_prototype.js
@@ -0,0 +1,26 @@
+// aqui farei o mesmo codigo só que organizado da (maneira certa).
+//aqui vai ficar só dados.
+function Pessoa(nome, idade, rg){
+    this.nome = nome
+    this.idade = idade
+    this.rg = rg
+}
+
+//basicamente vc tira as funções ou "metodos" de dentro do construtor.
+Pessoa.prototype.somar = function (a,b) {
+    return a + b
+}
+Pessoa.prototype.reclamar = () =>{
+    console.log('ain, uso das IAs vai acabar com a carreira de programador...')
+}
+Pessoa.prototype.perguntaIdiota = () =>{
+    console.log('1 + 1 é 2 ou 3')
+    console.log(Pessoa.prototype.somar(1, 1))
+}
+
+
+
+let pessoa1 = new Pessoa('Rodrigo', 21, true)
+console.log(pessoa1)
+pessoa1.reclamar()
+pessoa1.perguntaIdiota()
