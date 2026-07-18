5019340f19fbe04eab568e0b23e8eb0abab723b6
Marlon Marques <marlonmarques2002@gmail.com>
2026-07-18 17:33:01 -0300

    voltando a estudar

---
+++ testes_pessoais/ex/6_POO_ex.js
@@ -0,0 +1,35 @@
+//sistema de poupança automatica, todo deposito irá direto para poupança, vai precisar ser transferido para corrente para ser usado.
+class Conta{
+    constructor(saldoC, saldoP, jurosP){
+        this.saldoC = saldoC
+        this.saldoP = saldoP
+        // 1,4% ao mês
+        this.jurosP = jurosP
+    }
+    deposito(quantia){
+        if(quantia < 1){
+            return console.log("quantia invalida ou inexistente")
+        }
+        let total = this.saldoP += quantia
+        console.log(`Você depositou ${quantia}, seu saldo atual é ${total}`)
+    }
+    saque(conta = 'poupanca', quantia){
+        if(quantia < 1){
+            return console.log("quantia invalida ou inexistente")
+        }
+        if(conta == 'corrente'){
+            if(quantia > this.saldoC){
+                let total = this.saldoP -= quantia
+                console.log(`Sua conta corrente não tem saldo o suficiente, sacando da conta poupança...`)
+                console.log(`Você sacou ${quantia}, seu saldo atual é ${total}`)
+            } else{
+                if(quantia > this.saldoP){
+                    let total = this.saldoP -= quantia
+                    console.log(`Você sacou ${quantia}, seu saldo atual é ${total}`)
+                }else {
+                    console.log('Sem saldo suficiente na poupança.')
+                }
+            }
+        }
+    }
+}
